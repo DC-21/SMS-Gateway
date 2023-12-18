ff48165f06ab888c020efdf471732e994a63e776
dc-21 <cholahkuboko@gmail..com>
2023-12-18 11:18:12 +0200

    c

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,13 @@
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <section className=" min-h-screen w-full flex items-center justify-center px-4">
+      <main className=" border shadow-md p-4 flex flex-col gap-2 rounded-xl">
+        <h1>Login</h1>
+        <input type="email" className=" p-2 border rounded-md" />
+        <input type="password" className=" p-2 border rounded-md" />
+      </main>
+    </section>
+  );
 };
 
 export default Login;
