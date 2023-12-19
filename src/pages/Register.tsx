5c466b697e29307dd7ef996a519b8cc1c6541cee
dc-21 <cholahkuboko@gmail..com>
2023-12-19 15:57:37 +0200

    c

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -11,6 +11,7 @@ const Register = () => {
     register,
     handleSubmit,
     formState: { errors },
+    watch,
   } = useForm();
 
   const userLogin = async (data: any) => {
@@ -40,11 +41,11 @@ const Register = () => {
                 message: "Name shouldn't be less than 1 character",
               },
             })}
-            aria-invalid={errors.email ? "true" : "false"}
+            aria-invalid={errors.name ? "true" : "false"}
           />
-          {errors.email && (
+          {errors.name && (
             <span className="text-red-500">
-              {errors.email.message as string}
+              {errors.name?.message as string}
             </span>
           )}
         </div>
@@ -135,6 +136,70 @@ const Register = () => {
           {errors.password && (
             <span className="text-red-500">
               {errors.password.message as string}
+            </span>
+          )}
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <div className=" border rounded-md items-center flex w-full">
+            <input
+              type={showPass ? "text" : "password"}
+              className="p-2 focus:outline-none flex-1 bg-gray-200"
+              placeholder="Confirm Password"
+              {...register("confirmPassword", {
+                required: true,
+                validate: (value) =>
+                  value === watch("password") || "Passwords do not match",
+              })}
+              aria-invalid={errors.confirmPassword ? "true" : "false"}
+            />
+            <button
+              type="button"
+              onClick={() => setShowPass(!showPass)}
+              className=" px-2 hover:text-blue-500 duration-300"
+            >
+              {showPass ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+
+          {errors.confirmPassword && (
+            <span className="text-red-500">
+              {errors.confirmPassword?.message as string}
             </span>
           )}
         </div>
