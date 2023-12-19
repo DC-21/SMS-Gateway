7a5d6beda5b2468d1fb018fb3f4e06c53e89a050
dc-21 <cholahkuboko@gmail..com>
2023-12-19 16:50:25 +0200

    c

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,26 +1,28 @@
 const Dashboard = () => {
   return (
     <div className="w-full flex justify-center items-center p-4">
-      <div className="w-full grid md:grid-cols-2 grid-cols-1 items-center gap-4">
-        <div className="w-full flex justify-center border border-slate-300 shadow-md py-6 px-2 rounded">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m7.875 14.25 1.214 1.942a2.25 2.25 0 0 0 1.908 1.058h2.006c.776 0 1.497-.4 1.908-1.058l1.214-1.942M2.41 9h4.636a2.25 2.25 0 0 1 1.872 1.002l.164.246a2.25 2.25 0 0 0 1.872 1.002h2.092a2.25 2.25 0 0 0 1.872-1.002l.164-.246A2.25 2.25 0 0 1 16.954 9h4.636M2.41 9a2.25 2.25 0 0 0-.16.832V12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 12V9.832c0-.287-.055-.57-.16-.832M2.41 9a2.25 2.25 0 0 1 .382-.632l3.285-3.832a2.25 2.25 0 0 1 1.708-.786h8.43c.657 0 1.281.287 1.709.786l3.284 3.832c.163.19.291.404.382.632M4.5 20.25h15A2.25 2.25 0 0 0 21.75 18v-2.625c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125V18a2.25 2.25 0 0 0 2.25 2.25Z"
-            />
-          </svg>
-          Remaining SMS: 200
+      <div className="w-full grid md:grid-cols-2 text-lg grid-cols-1 items-center gap-4">
+        <div className="w-full flex flex-col gap-2 items-center justify-center border border-slate-300 shadow-md py-6 px-2 rounded">
+          <div className=" flex items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 text-blue-700"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m7.875 14.25 1.214 1.942a2.25 2.25 0 0 0 1.908 1.058h2.006c.776 0 1.497-.4 1.908-1.058l1.214-1.942M2.41 9h4.636a2.25 2.25 0 0 1 1.872 1.002l.164.246a2.25 2.25 0 0 0 1.872 1.002h2.092a2.25 2.25 0 0 0 1.872-1.002l.164-.246A2.25 2.25 0 0 1 16.954 9h4.636M2.41 9a2.25 2.25 0 0 0-.16.832V12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 12V9.832c0-.287-.055-.57-.16-.832M2.41 9a2.25 2.25 0 0 1 .382-.632l3.285-3.832a2.25 2.25 0 0 1 1.708-.786h8.43c.657 0 1.281.287 1.709.786l3.284 3.832c.163.19.291.404.382.632M4.5 20.25h15A2.25 2.25 0 0 0 21.75 18v-2.625c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125V18a2.25 2.25 0 0 0 2.25 2.25Z"
+              />
+            </svg>
+            Remaining SMS
+          </div>
+          <span className=" text-4xl font-bold text-blue-500">200</span>
         </div>
-        <div className="w-full flex justify-center border border-slate-300 shadow-md py-6 px-2 rounded">
-          Used SMS: 1800
+        <div className="w-full flex flex-col gap-2 items-center justify-center border border-slate-300 shadow-md py-6 px-2 rounded">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -36,8 +38,9 @@ const Dashboard = () => {
               d="M21.75 9v.906a2.25 2.25 0 0 1-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 0 0 1.183 1.981l6.478 3.488m8.839 2.51-4.66-2.51m0 0-1.023-.55a2.25 2.25 0 0 0-2.134 0l-1.022.55m0 0-4.661 2.51m16.5 1.615a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V8.844a2.25 2.25 0 0 1 1.183-1.981l7.5-4.039a2.25 2.25 0 0 1 2.134 0l7.5 4.039a2.25 2.25 0 0 1 1.183 1.98V19.5Z"
             />
           </svg>
+          Used SMS: 1800
         </div>
-        <div className="w-full flex justify-center border border-slate-300 shadow-md py-6 px-2 rounded">
+        <div className="w-full flex flex-col gap-2 items-center justify-center border border-slate-300 shadow-md py-6 px-2 rounded">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -54,7 +57,7 @@ const Dashboard = () => {
           </svg>
           Pending SMS: 12
         </div>
-        <div className="w-full flex justify-center border border-slate-300 shadow-md py-6 px-2 rounded">
+        <div className="w-full flex flex-col gap-2 items-center justify-center border border-slate-300 shadow-md py-6 px-2 rounded">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
